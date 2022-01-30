b649cd2bf519bb837aade396df6762bc105564e4
Ahmadum01 <ahmad.um.2001@mail.ru>
2022-01-30 19:20:44 +0300

    some review

--- Python Core/Password Hacker/hack.py
+++ Python Core/Password Hacker/hack.py
@@ -9,12 +9,12 @@ ADDRESS = (ARGS[1], int(ARGS[2]))
 
 with socket.socket() as client_socket, open('logins.txt', 'r') as logins:  # try to pick up login
     client_socket.connect(ADDRESS)
-    is_broken = False  # For break loop
-    right_login, password = '', ''
     for login in [i.strip() for i in logins]:
         client_socket.send(json.dumps({'login': login, 'password': ''}).encode())
         if json.loads(client_socket.recv(1000).decode())['result'] == 'Wrong password!':
             break
+    is_broken = False  # For break loop
+    password = ''
     while not is_broken:
         # trying to pick up one char from password
         for letter in string.ascii_letters + string.digits:
